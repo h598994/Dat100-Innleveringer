fca7dc72171b4d4e98edf494cc35eec9b01afd0f
stian <stian@192.168.1.134>
2021-09-12 14:47:38 +0200

    Endret en feil i oppgave 2

--- Innlevering uke 36/src/no/hvl/dat100/Oblig02.java
+++ Innlevering uke 36/src/no/hvl/dat100/Oblig02.java
@@ -13,15 +13,15 @@ public class Oblig02 {
 
 		if (userTestScore < 39 && userTestScore >= 0) { 
 			showMessageDialog(null,"F"); 
-		} else if (userTestScore < 49 && userTestScore >= 40) { 
+		} else if (userTestScore <= 49 && userTestScore >= 40) { 
 			showMessageDialog(null,"E"); 
-		} else if (userTestScore < 59 && userTestScore >= 50) { 
+		} else if (userTestScore <= 59 && userTestScore >= 50) { 
 			showMessageDialog(null,"D"); 
-		} else if (userTestScore < 79 && userTestScore >= 60) { 
+		} else if (userTestScore <= 79 && userTestScore >= 60) { 
 			showMessageDialog(null,"C"); 
-		} else if (userTestScore < 89 && userTestScore >= 80) { 
+		} else if (userTestScore <= 89 && userTestScore >= 80) { 
 			showMessageDialog(null,"B");	 
-		} else if (userTestScore < 100 && userTestScore >= 90) { 
+		} else if (userTestScore <= 100 && userTestScore >= 90) { 
 			showMessageDialog(null,"A"); 
 		} else { 
 			showMessageDialog(null,"Error 404"); 
